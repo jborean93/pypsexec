4fd5d4d36a7f341f148c8e42f949c5f36f094a27
Jordan Borean <jborean93@gmail.com>
2018-02-19 16:42:41 +1000

    removed logging from __init__.py

--- pypsexec/__init__.py
+++ pypsexec/__init__.py
@@ -10,14 +10,4 @@ except ImportError:
 logger = logging.getLogger(__name__)
 logger.addHandler(NullHandler())
 
-# TODO: used for development, remove before release
-logger.setLevel(logging.DEBUG)
-
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - '
-                              '%(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
-
 __version__ = '0.0.1'
